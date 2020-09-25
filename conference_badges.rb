79f5b8f075d3febb9008a6e6e6cc6e200672d829
Alan Tang <giabao_tang@yahoo.com>
2020-09-25 01:13:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,11 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end  
 
-def batch_badge_creator( attendees)
-  
+def batch_badge_creator(attendees)
+  list = []
+  attendees.each do |attendee|
+    list << badge_maker(attendee)
+  end
+  list
+    
 end  
